89915bfd23b278dc1e398fa3f2e6eb3212b99482
shahxa99 <129101720+shahxa99@users.noreply.github.com>
2023-08-22 16:12:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+let previousEl = document.getElementById("previous-el")
+let totalEntriesEl = document.getElementById("total-entries-el")
+let countEl = document.getElementById("countPeople")
+
+let count = 0
+
 function increment() {
-    console.log("yo man")
+    count += 1
+    countEl.textContent = count
+}
+let totalCount = 0 
+
+function save() {
+
+    let entries = count + " - " 
+    previousEl.textContent += entries
+
+    totalCount += count
+    let totalEntrirs = " " + totalCount
+    totalEntriesEl.textContent = totalEntrirs
+    countEl.textContent = 0
+    count = 0
 }
